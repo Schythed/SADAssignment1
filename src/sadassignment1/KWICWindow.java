0e928c0a67f386f7a2644d53bd63592720821a55
josh <josh@josh-GE70-2OC-2OD-2OE>
2017-06-19 13:53:15 -0500

    -Deleted header comments

--- src/sadassignment1/KWICWindow.java
+++ src/sadassignment1/KWICWindow.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sadassignment1;
 
 import java.awt.Dimension;
@@ -19,6 +14,10 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
 
+/**
+ *
+ * @author josh
+ */
 class KWICWindow extends JFrame
 {
     private final JTextArea inputBox, outputBox;
